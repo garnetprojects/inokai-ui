2966ce82cc0b594f9de29731808d7ce3acb742e2
garnetprojects <services@garnetprojects.com>
2024-10-04 09:39:37 +0200

    Update Calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -55,41 +55,43 @@ const Calendar = ({ data, setOpen, selectedDate }) => {
           let availibity = bringAvailibity(user.user_id, data?.appointments2);
 
           return (
-            <Tooltip
-              title={`${availibity.from ? availibity.from : ''}  ${
-                availibity.to ? `a ${availibity.to}` : ''
-              }`}
-              arrow
-            >
-              <Box
-                key={user._id}
-                bgcolor={'white'}
-                flex={'1'}
-                // width={'100%'}
-              >
-                <Box
-                  display={'flex'}
-                  // maxWidth={200}
-                  mx={'auto'}
-                  border={'1px solid #e0e0e0'}
-                  py={1}
-                  px={'10px'}
-                >
-                  <Box mx={1} textTransform={'uppercase'}>
-                    <Avatar>{user.name[0]}</Avatar>
-                  </Box>
+           <Tooltip
+  title={`${availibity.from ? availibity.from : ''}  ${
+    availibity.to ? `a ${availibity.to}` : ''
+  }`}
+  arrow
+>
+  <Box
+    key={user._id}
+    bgcolor={'white'}
+    flex={'1'}
+  >
+    <Box
+      display={'flex'}
+      mx={'auto'}
+      border={'1px solid #e0e0e0'}
+      py={1}
+      px={'10px'}
+      flexDirection={'row'} // Keep horizontal for avatar + text
+    >
+      <Box mx={1} textTransform={'uppercase'}>
+        <Avatar>{user.name[0]}</Avatar>
+      </Box>
 
-                  <Typography variant="body2" whiteSpace={'nowrap'}>
-                    {user.name}
-                  </Typography>
-                  <Typography variant="body2" whiteSpace={'nowrap'}>
-                  {`${availibity.from ? availibity.from : ''}  ${
-                availibity.to ? `a ${availibity.to}` : ''
-              }`}
-                  </Typography>
-                </Box>
-              </Box>
-            </Tooltip>
+      <Box display={'flex'} flexDirection={'column'}> {/* Stack typography vertically */}
+        <Typography variant="body2" whiteSpace={'nowrap'}>
+          {user.name}
+        </Typography>
+        <Typography variant="body2" whiteSpace={'nowrap'}>
+          {`${availibity.from ? availibity.from : ''}  ${
+            availibity.to ? `a ${availibity.to}` : ''
+          }`}
+        </Typography>
+      </Box>
+    </Box>
+  </Box>
+</Tooltip>
+
           );
         })}
       </Box>
