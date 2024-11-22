082d38d8177403ddc367510ddf8975948809cf17
rocatechnologies <services@garnetprojects.com>
2024-11-22 13:51:39 +0100

    minor

--- src/pages/Horarios.jsx
+++ src/pages/Horarios.jsx
@@ -321,7 +321,7 @@ const Horarios = () => {
                 label="Empleado"
                 aditionalProperties={{
                   onChange: (e) => handleManualChange('employee', e.target.value),
-                  value: manualData.employee,
+                  value: manualData.user,
                 }}
                 required={true}
                 sx={{ mb: 2 }}
