5d30ad8768082178e65e7200e7655609651c6e5b
rocatechnologies <services@garnetprojects.com>
2024-11-22 14:31:17 +0100

    minor

--- src/pages/Horarios.jsx
+++ src/pages/Horarios.jsx
@@ -67,7 +67,7 @@ const Horarios = () => {
   
     try {
       const response = await axios.post(
-        `/horario-manual/${dataBase}`, // Reemplaza 'mySelectedDB' con el nombre de la base de datos
+        `/Sappointment/horario-manual/${dataBase}`, // Reemplaza 'mySelectedDB' con el nombre de la base de datos
         manualEntry
       );
   
