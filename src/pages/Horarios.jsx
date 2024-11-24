d3ace39bf81a3a02b769a2944e12b7821198c1ed
rocatechnologies <services@garnetprojects.com>
2024-11-24 13:37:24 +0100

    minor

--- src/pages/Horarios.jsx
+++ src/pages/Horarios.jsx
@@ -22,7 +22,7 @@ import { fixCentersArray, fixUserArray } from '../utils/fixArray';
 import Grid from '@mui/material/Unstable_Grid2';
 import SelectComponent from '../components/SelectComponent';
 import { Close } from '@mui/icons-material';
-import { FormControlLabel } from '@mui/material';
+import { Checkbox, FormControlLabel, FormGroup } from '@mui/material';
 
 const Horarios = () => {
   const [t] = useTranslation('global');
